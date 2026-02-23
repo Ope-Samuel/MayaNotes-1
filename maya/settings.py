4fd57a4bb82c29c5c4dea9069de096eb9550d7f7
Ope-Samuel <temitopeademuwagun150@gmail.com>
2026-02-23 14:17:27 +0100

    Ready for Railway

--- maya/settings.py
+++ maya/settings.py
@@ -73,12 +73,7 @@ WSGI_APPLICATION = 'maya.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {}
 
 
 # Password validation
@@ -115,11 +110,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS=['static/']
-MEDIA_URL = 'media/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_URL = '/static/'
+#STATICFILES_DIRS=['static/']
+#STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+#MEDIA_URL = 'media/'
+#MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Optional: for WhiteNoise (already added if you followed instructions)
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
